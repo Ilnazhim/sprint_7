134813e8a3b1345d550b7ab869d169aa75de5ed5
iLnazhim <bass_boss@mail.ru>
2024-05-30 13:03:17 +0500

    add allure steps

--- modules/basic_methods.py
+++ modules/basic_methods.py
@@ -1,34 +1,38 @@
 import random
 import string
+
+import allure
 import requests
 from src import urls
 
 
-def return_login_password():
-    def generate_random_string(length):
-        letters = string.ascii_lowercase
-        random_string = ''.join(random.choice(letters) for _ in range(length))
-        return random_string
+with allure.step("Получение рандомного логина, пароля и имени курьера"):
+    def return_login_password():
+        def generate_random_string(length):
+            letters = string.ascii_lowercase
+            random_string = ''.join(random.choice(letters) for _ in range(length))
+            return random_string
 
-    login = generate_random_string(10)
-    password = generate_random_string(10)
-    first_name = generate_random_string(10)
+        login = generate_random_string(10)
+        password = generate_random_string(10)
+        first_name = generate_random_string(10)
 
-    payload = {
-        "login": login,
-        "password": password,
-        "firstName": first_name
-    }
+        payload = {
+            "login": login,
+            "password": password,
+            "firstName": first_name
+        }
 
-    return payload
+        return payload
 
 
-def login_courier_and_return_id():
-    ENDPOINT = '/courier/login'
-    data = {
-        "login": 'ilnazhim',
-        "password": 'sprint_7'
-    }
-    response = requests.post(urls.URL + ENDPOINT, data=data)
-    assert response.status_code == 200
-    return response.json()['id']
+with allure.step("Получение id курьера"):
+    def login_courier_and_return_id():
+        ENDPOINT = '/courier/login'
+        data = {
+            "login": 'ilnazhim',
+            "password": 'sprint_7'
+        }
+        response = requests.post(urls.URL + ENDPOINT, data=data)
+        assert response.status_code == 200
+        return response.json()['id']
